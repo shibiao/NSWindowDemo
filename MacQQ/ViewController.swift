314e40b16d3b5a9661d40f3d1de3874004cd5838
WalkWithWind <956035125@qq.com>
2017-12-15 08:40:07 +0800

    Initial Commit

---
+++ MacQQ/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacQQ
+//
+//  Created by sycf_ios on 2017/12/15.
+//  Copyright © 2017年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
